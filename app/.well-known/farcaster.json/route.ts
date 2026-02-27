83fb1282ed884fcfdada11e11d2904dc5b6cbafa
IanoNJuguna <inc3099@gmail.com>
2026-02-27 21:13:41 +0300

    feat: redirect .well-known/farcaster.json to Farcaster hosted manifest

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,24 +1,6 @@
 export async function GET() {
-	const URL = 'https://doba.world';
-	return Response.json({
-		accountAssociation: {
-			header: 'eyJmaWQiOjExNjQzNjgsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhlNzQyQzAxZTdEOTUyMUE0NDJkODJmODk5NUI3MkEyODE5MGFEMzJFIn0',
-			payload: 'eyJkb21haW4iOiJkb2JhLndvcmxkIn0',
-			signature: 'voST/MQHkJ2wSOUgWiyjKWuXH5wMuvbSKlp3jRAzm3koo9/yJ96pB61AXwVrVf2Q6Aj0S+ONhZx8MYdDoI61zRw=',
-		},
-		frame: {
-			version: '1',
-			name: 'doba',
-			homeUrl: URL,
-			iconUrl: `${URL}/logo.png`,
-			splashImageUrl: `${URL}/doba-preview.png`,
-			splashBackgroundColor: '#0D0D12',
-			webhookUrl: `${URL}/api/webhook`,
-		},
-	}, {
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Methods': 'GET',
-		}
-	});
+	return Response.redirect(
+		'https://api.farcaster.xyz/miniapps/hosted-manifest/019ca04d-2024-4d34-4107-1179fe02bfc7',
+		307
+	)
 }
